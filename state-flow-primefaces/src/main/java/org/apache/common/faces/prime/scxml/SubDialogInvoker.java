6bf61cce462a490af81058ef5188f25f0c6b7a94
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2018-03-31 16:42:41 +0200

    Create Java Chart Flow Faces

--- state-flow-primefaces/src/main/java/org/apache/common/faces/prime/scxml/SubDialogInvoker.java
+++ state-flow-primefaces/src/main/java/org/apache/common/faces/prime/scxml/SubDialogInvoker.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
