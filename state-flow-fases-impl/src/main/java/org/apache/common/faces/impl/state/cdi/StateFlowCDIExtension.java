12291cfcc7d815f650e824d3e424c5ff1dd2a7c0
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2018-04-08 23:25:46 +0200

    Create IDM system

--- state-flow-fases-impl/src/main/java/org/apache/common/faces/impl/state/cdi/StateFlowCDIExtension.java
+++ state-flow-fases-impl/src/main/java/org/apache/common/faces/impl/state/cdi/StateFlowCDIExtension.java
@@ -101,9 +101,9 @@ public class StateFlowCDIExtension implements Extension {
         event.addContext(new StateCDIContext(targetScopedBeanFlowIds));
         targetScopedBeanFlowIds.clear();
 
-        event.addBean(new DialogContextProducer());
-        event.addBean(new ChartContextProducer());
-        event.addBean(new StateContextProducer());
+//        event.addBean(new DialogContextProducer());
+//        event.addBean(new ChartContextProducer());
+//        event.addBean(new StateContextProducer());
         
         event.addBean(new PathResolverProducer());
         event.addBean(new ExecutorResolverProducer());
