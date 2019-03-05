a64b603939dbc491c5246ad141886eafd776b8f7
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2019-03-05 15:25:39 +0100

    Create Portal Application

--- state-flow-fases-impl/src/main/java/org/apache/common/faces/impl/state/facelets/StateFlowPhaseListener.java
+++ state-flow-fases-impl/src/main/java/org/apache/common/faces/impl/state/facelets/StateFlowPhaseListener.java
@@ -16,7 +16,9 @@
 package org.apache.common.faces.impl.state.facelets;
 
 import static com.sun.faces.util.RequestStateManager.FACES_VIEW_STATE;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -26,6 +28,9 @@ import java.util.logging.Logger;
 import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIViewRoot;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitHint;
+import javax.faces.component.visit.VisitResult;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
 import javax.faces.event.PhaseEvent;
@@ -36,6 +41,7 @@ import org.apache.common.faces.state.component.UIStateChartDefinition;
 import org.apache.common.faces.state.StateFlowHandler;
 import org.apache.common.scxml.model.SCXML;
 import org.apache.common.faces.impl.state.StateFlowParams;
+import static org.apache.common.faces.impl.state.facelets.StateFlowControllerListener.getControllerClientIds;
 import org.apache.common.faces.state.StateFlow;
 import static org.apache.common.faces.state.StateFlow.AFTER_PHASE_EVENT_PREFIX;
 import static org.apache.common.faces.state.StateFlow.BEFORE_PHASE_EVENT_PREFIX;
@@ -49,6 +55,7 @@ import org.apache.common.scxml.EventDispatcher;
 import org.apache.common.scxml.TriggerEvent;
 import org.apache.common.scxml.model.ModelException;
 import static org.apache.common.faces.state.StateFlow.DECODE_DISPATCHER_EVENTS;
+import org.apache.common.faces.state.component.UIStateChartController;
 
 /**
  *
@@ -58,39 +65,66 @@ public class StateFlowPhaseListener implements PhaseListener {
 
     @Override
     public void afterPhase(PhaseEvent event) {
-        FacesContext context = event.getFacesContext();
+        FacesContext facesContext = event.getFacesContext();
 
         if (event.getPhaseId() == PhaseId.RESTORE_VIEW) {
-            restoreStateFlow(context);
+            restoreStateFlow(facesContext);
         }
 
-        UIViewRoot viewRoot = context.getViewRoot();
+        UIViewRoot viewRoot = facesContext.getViewRoot();
         StateFlowHandler handler = StateFlowHandler.getInstance();
-        if (viewRoot != null && handler.isActive(context)) {
-
-            SCXMLExecutor executor = handler.getRootExecutor(context);
+        if (viewRoot != null) {
 
             String name = AFTER_PHASE_EVENT_PREFIX
                     + event.getPhaseId().getName().toLowerCase();
 
-            EventBuilder eb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
-                    .sendId(context.getViewRoot().getViewId());
+            if (handler.isActive(facesContext)) {
+                SCXMLExecutor executor = handler.getRootExecutor(facesContext);
 
-            try {
-                executor.triggerEvent(eb.build());
-            } catch (ModelException ex) {
-                throw new FacesException(ex);
+                EventBuilder eb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
+                        .sendId(facesContext.getViewRoot().getViewId());
+
+                try {
+                    executor.triggerEvent(eb.build());
+                } catch (ModelException ex) {
+                    throw new FacesException(ex);
+                }
+
+                if (!executor.isRunning()) {
+                    handler.close(facesContext, executor);
+                }
+
+                Context.setCurrentInstance(
+                        (Context) facesContext.getELContext().getContext(Context.class));
+
+                if (event.getPhaseId() == PhaseId.RENDER_RESPONSE || facesContext.getResponseComplete()) {
+                    handler.writeState(facesContext);
+                }
             }
 
-            if (!executor.isRunning()) {
-                handler.close(context, executor);
-            }
+            ArrayList<String> clientIds = getControllerClientIds(facesContext);
+            if (clientIds != null && !clientIds.isEmpty()) {
+                Set<VisitHint> hints = EnumSet.of(VisitHint.SKIP_ITERATION);
+                VisitContext visitContext = VisitContext.createVisitContext(facesContext, clientIds, hints);
+                viewRoot.visitTree(visitContext, (VisitContext context, UIComponent target) -> {
+                    if (target instanceof UIStateChartController) {
+                        UIStateChartController controller = (UIStateChartController) target;
+                        String controllerId = controller.getClientId(facesContext);
 
-            Context.setCurrentInstance(
-                    (Context) context.getELContext().getContext(Context.class));
+                        EventBuilder veb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
+                                .sendId(controllerId);
 
-            if (event.getPhaseId() == PhaseId.RENDER_RESPONSE || context.getResponseComplete()) {
-                handler.writeState(context);
+                        SCXMLExecutor rootExecutor = controller.getRootExecutor(facesContext);
+                        if (rootExecutor != null) {
+                            try {
+                                rootExecutor.triggerEvent(veb.build());
+                            } catch (ModelException ex) {
+                                throw new FacesException(ex);
+                            }
+                        }
+                    }
+                    return VisitResult.ACCEPT;
+                });
             }
 
         }
@@ -99,66 +133,93 @@ public class StateFlowPhaseListener implements PhaseListener {
 
     @Override
     public void beforePhase(PhaseEvent event) {
-        FacesContext context = event.getFacesContext();
+        FacesContext facesContext = event.getFacesContext();
 
         if (event.getPhaseId() != PhaseId.RESTORE_VIEW) {
-            UIViewRoot viewRoot = context.getViewRoot();
+            UIViewRoot viewRoot = facesContext.getViewRoot();
             StateFlowHandler handler = StateFlowHandler.getInstance();
-            if (viewRoot != null && handler.isActive(context)) {
-
-                StateFlow.applyViewContext(context);
+            if (viewRoot != null) {
 
                 String name = BEFORE_PHASE_EVENT_PREFIX
                         + event.getPhaseId().getName().toLowerCase();
 
-                EventBuilder eb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
-                        .sendId(viewRoot.getViewId());
+                if (handler.isActive(facesContext)) {
+                    StateFlow.applyViewContext(facesContext);
 
-                SCXMLExecutor executor = handler.getRootExecutor(context);
-                try {
-                    executor.triggerEvent(eb.build());
-                } catch (ModelException ex) {
-                    throw new FacesException(ex);
-                }
+                    EventBuilder eb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
+                            .sendId(viewRoot.getViewId());
 
-                if (event.getPhaseId() == PhaseId.APPLY_REQUEST_VALUES
-                        && !context.getResponseComplete()) {
-                    EventDispatcher ed = executor.getEventdispatcher();
-                    if (ed instanceof FacesProcessHolder) {
-                        try {
-                            EventBuilder eeb = new EventBuilder(DECODE_DISPATCHER_EVENTS,
-                                    TriggerEvent.CALL_EVENT)
-                                    .sendId(viewRoot.getViewId());
+                    SCXMLExecutor executor = handler.getRootExecutor(facesContext);
+                    try {
+                        executor.triggerEvent(eb.build());
+                    } catch (ModelException ex) {
+                        throw new FacesException(ex);
+                    }
 
-                            executor.triggerEvent(eeb.build());
-                            ((FacesProcessHolder) ed).processDecodes(context);
-                        } catch (ModelException ex) {
-                            throw new FacesException(ex);
+                    if (event.getPhaseId() == PhaseId.APPLY_REQUEST_VALUES
+                            && !facesContext.getResponseComplete()) {
+                        EventDispatcher ed = executor.getEventdispatcher();
+                        if (ed instanceof FacesProcessHolder) {
+                            try {
+                                EventBuilder eeb = new EventBuilder(DECODE_DISPATCHER_EVENTS,
+                                        TriggerEvent.CALL_EVENT)
+                                        .sendId(viewRoot.getViewId());
+
+                                executor.triggerEvent(eeb.build());
+                                ((FacesProcessHolder) ed).processDecodes(facesContext);
+                            } catch (ModelException ex) {
+                                throw new FacesException(ex);
+                            }
                         }
+                    }
+
+                    if (!executor.isRunning()) {
+                        handler.close(facesContext, executor);
                     }
                 }
 
-                if (!executor.isRunning()) {
-                    handler.close(context, executor);
+                ArrayList<String> clientIds = getControllerClientIds(facesContext);
+                if (clientIds != null && !clientIds.isEmpty()) {
+                    Set<VisitHint> hints = EnumSet.of(VisitHint.SKIP_ITERATION);
+                    VisitContext visitContext = VisitContext.createVisitContext(facesContext, clientIds, hints);
+                    viewRoot.visitTree(visitContext, (VisitContext context, UIComponent target) -> {
+                        if (target instanceof UIStateChartController) {
+                            UIStateChartController controller = (UIStateChartController) target;
+                            String controllerId = controller.getClientId(facesContext);
+
+                            EventBuilder veb = new EventBuilder(name, TriggerEvent.CALL_EVENT)
+                                    .sendId(controllerId);
+
+                            SCXMLExecutor rootExecutor = controller.getRootExecutor(facesContext);
+                            if (rootExecutor != null) {
+                                try {
+                                    rootExecutor.triggerEvent(veb.build());
+                                } catch (ModelException ex) {
+                                    throw new FacesException(ex);
+                                }
+                            }
+                        }
+                        return VisitResult.ACCEPT;
+                    });
                 }
 
             } else if (event.getPhaseId() == PhaseId.APPLY_REQUEST_VALUES
-                    && !context.getResponseComplete()) {
+                    && !facesContext.getResponseComplete()) {
 
-                if (handler.isFinal(context)) {
-                    handler.close(context);
+                if (handler.isFinal(facesContext)) {
+                    handler.close(facesContext);
                 }
             }
 
         } else {
             StateFlowHandler handler = StateFlowHandler.getInstance();
-            if (handler.isActive(context)) {
-                SCXMLExecutor executor = handler.getRootExecutor(context);
+            if (handler.isActive(facesContext)) {
+                SCXMLExecutor executor = handler.getRootExecutor(facesContext);
                 Context ctx = executor.getRootContext();
                 Object lastViewState = ctx.get(FACES_VIEW_STATE);
                 ctx.getVars().remove(FACES_VIEW_STATE);
                 if (lastViewState != null) {
-                    context.getAttributes().put(FACES_VIEW_STATE, lastViewState);
+                    facesContext.getAttributes().put(FACES_VIEW_STATE, lastViewState);
                 }
             }
         }
