5863e27ac8d259dc18695c64de385bbc05838bbd
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2019-03-14 16:22:41 +0100

    Create Portal Application

--- state-flow-fases-api/src/main/java/org/apache/common/faces/state/scxml/model/If.java
+++ state-flow-fases-api/src/main/java/org/apache/common/faces/state/scxml/model/If.java
@@ -73,6 +73,7 @@ public class If extends Action implements ActionsContainer {
      *
      * @return Returns the actions.
      */
+    @Override
     public final List<Action> getActions() {
         return actions;
     }
@@ -83,6 +84,7 @@ public class If extends Action implements ActionsContainer {
      *
      * @param action The action to add.
      */
+    @Override
     public final void addAction(final Action action) {
         if (action != null) {
             this.actions.add(action);
